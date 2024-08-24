933ec7166453554e9c549821d700dbba53a9fc15
asahaida <156024925+asahaida@users.noreply.github.com>
2024-08-24 17:06:20 -0500

    Create index.js

---
+++ Street Dog Case Study/index.js
@@ -0,0 +1,52 @@
+function fade() {
+    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
+    
+    $(".searchButton").on("click", function(){
+      $(".searchButton").toggleClass("active");
+      if ($(".searchButton").hasClass("active")) {
+        $(".searchBar").css("height", "100vh");
+        $("#searchForm").css("opacity","1");
+        $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+      }
+      else {
+        $(".searchBar").css('height', "0vh");
+        $("#searchForm").css("opacity","0");
+        $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+      }
+    });
+    
+    $(".toggle").click(function(){
+      $(this).find(".toggleContainer").toggleClass("active");
+      $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+    });
+     
+    var acc = document.getElementsByClassName("accordion");
+    
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+    
+        this.classList.toggle("active");
+    
+    
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+
+    // Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
