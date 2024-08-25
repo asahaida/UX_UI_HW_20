1dd9c5fba342caaef7bcdf3812dd2dc9355fb48f
asahaida <156024925+asahaida@users.noreply.github.com>
2024-08-24 19:27:03 -0500

    removing unneeded code

--- Street Dog Case Study/index.js
+++ Street Dog Case Study/index.js
@@ -1,18 +1,12 @@
-function fade() {
-    $('.preloader').fadeOut("slow");
-    }
-    setTimeout(fade, 3000);
-    
+
     $(".searchButton").on("click", function(){
       $(".searchButton").toggleClass("active");
       if ($(".searchButton").hasClass("active")) {
         $(".searchBar").css("height", "100vh");
-        $("#searchForm").css("opacity","1");
         $(".searchIcon").removeClass("fa-search").addClass("fa-times");
       }
       else {
         $(".searchBar").css('height', "0vh");
-        $("#searchForm").css("opacity","0");
         $(".searchIcon").removeClass("fa-times").addClass("fa-search");
       }
     });
@@ -21,26 +15,6 @@ function fade() {
       $(this).find(".toggleContainer").toggleClass("active");
       $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
     });
-     
-    var acc = document.getElementsByClassName("accordion");
-    
-    var i;
-    
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-    
-        this.classList.toggle("active");
-    
-    
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
-    }
-
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
